da68a952c74469f47b63658b3accdeb9d902015c
Sephinzo <aryayogaputra84@gmail.com>
2025-12-19 14:08:52 +0700

    minor adjust 3

--- KonversiMataUang.go
+++ KonversiMataUang.go
@@ -16,35 +16,38 @@ const (
 func main() {
 	var jumlah, usd, eur float64
 	var MataUang, errormsg string
-
-	fmt.Print("ketik mata uang yang akan dikonversi (IDR, PHP, THB, MYR, SGD) menggunakan huruf kapital: ")
+	fmt.Print("ketik mata uang yang akan dikonversi (IDR, PHP, THB, MYR, SGD) : ")
 	fmt.Scan(&MataUang)
-	fmt.Print("ketik jumlah mata uang yang akan dikonversi (tanpa titik atau koma): ")
-	fmt.Scan(&jumlah)
-	// cek nilai minus
-	if jumlah < 0 {
-		errormsg = "Jumlah mata uang tidak boleh kurang dari 0"
-	} else {
-		if MataUang == "IDR" {
-			usd = jumlah * idr_usd
-		} else if MataUang == "PHP" {
-			usd = jumlah * php_usd
-		} else if MataUang == "THB" {
-			usd = jumlah * thb_usd
-		} else if MataUang == "MYR" {
-			usd = jumlah * myr_usd
-		} else if MataUang == "SGD" {
-			usd = jumlah * sgd_usd
-		} else {
-			errormsg = "Mata uang yang anda masukkan tidak tersedia"
+
+	for jumlah <= 0 {
+		fmt.Print("ketik jumlah mata uang yang akan dikonversi (tanpa titik atau koma): ")
+		fmt.Scan(&jumlah)
+
+		if jumlah <= 0 {
+			fmt.Println("Jumlah mata uang ndak boleh minus")
 		}
 	}
+
+	if MataUang == "IDR" || MataUang == "idr" {
+		usd = jumlah * idr_usd
+	} else if MataUang == "PHP" || MataUang == "php" {
+		usd = jumlah * php_usd
+	} else if MataUang == "THB" || MataUang == "thb" {
+		usd = jumlah * thb_usd
+	} else if MataUang == "MYR" || MataUang == "myr" {
+		usd = jumlah * myr_usd
+	} else if MataUang == "SGD" || MataUang == "sgd" {
+		usd = jumlah * sgd_usd
+	} else {
+		errormsg = "Mata uang yang anda masukkan gak ada"
+	}
+
 	eur = usd * usd_eur
 
 	if errormsg == "" {
 		fmt.Printf("USD: $%.2f\n", usd)
 		fmt.Printf("EUR: €%.2f\n", eur)
 	} else {
-		fmt.Println("Gagal:", errormsg)
+		fmt.Println("izinn:", errormsg)
 	}
 }
