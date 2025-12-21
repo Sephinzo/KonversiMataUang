60cd60e6d63e71f1b16479650682d757517a1ca8
Sephinzo <132820612+Sephinzo@users.noreply.github.com>
2025-12-21 23:04:02 +0700

    Major changes 1

--- KonversiMataUang.go
+++ KonversiMataUang.go
@@ -1,7 +1,9 @@
 package main
+
 import (
 	"fmt"
 )
+
 const (
 	idr_usd = 0.00006
 	php_usd = 0.17
@@ -10,41 +12,82 @@ const (
 	sgd_usd = 0.77
 	usd_eur = 0.86
 )
+
 func main() {
-	var jumlah, usd, eur float64
-	var MataUang, errormsg string
-	fmt.Print("ketik mata uang yang akan dikonversi (IDR, PHP, THB, MYR, SGD) : ")
-	fmt.Scan(&MataUang)
+	var jumlah, usd float64
+	var outIDR, outPHP, outTHB, outMYR, outSGD, outUSD, outEUR float64
+	var MataUang string
+	var kontrolutama bool
+	var validasimatauang bool
+	
+	kontrolutama = true
+	
+	for kontrolutama {
+		validasimatauang = false
+		
+		for validasimatauang == false {
+			fmt.Print("Ketik mata uang asal (IDR, PHP, THB, MYR, SGD) atau ketik 'EXIT' untuk keluar: ")
+			fmt.Scan(&MataUang)
 
-	for jumlah <= 0 {
-		fmt.Print("ketik jumlah mata uang yang akan dikonversi (tanpa titik atau koma): ")
-		fmt.Scan(&jumlah)
+			if MataUang == "exit" || MataUang == "EXIT" {
+				kontrolutama = false
+				validasimatauang = true
+			} else {
+				if MataUang == "IDR" || MataUang == "idr" ||
+				   MataUang == "PHP" || MataUang == "php" ||
+				   MataUang == "THB" || MataUang == "thb" ||
+				   MataUang == "MYR" || MataUang == "myr" ||
+				   MataUang == "SGD" || MataUang == "sgd" {
+					validasimatauang = true
+				} else {
+					fmt.Println("Mata uang tidak ada, silahkan input kembali.")
+					validasimatauang = false
+				}
+			}
+		}
 
-		if jumlah <= 0 {
-			fmt.Println("Jumlah mata uang ndak boleh minus")
+		if kontrolutama == true {
+			jumlah = 0 
+			
+			for jumlah <= 0 {
+				fmt.Print("Ketik jumlah mata uang (harus lebih dari 0): ")
+				fmt.Scan(&jumlah)
+
+				if jumlah <= 0 {
+					fmt.Println("Jumlah mata uang tidak boleh minus atau nol")
+				}
+			}
+			
+			if MataUang == "IDR" || MataUang == "idr" {
+				usd = jumlah * idr_usd
+			} else if MataUang == "PHP" || MataUang == "php" {
+				usd = jumlah * php_usd
+			} else if MataUang == "THB" || MataUang == "thb" {
+				usd = jumlah * thb_usd
+			} else if MataUang == "MYR" || MataUang == "myr" {
+				usd = jumlah * myr_usd
+			} else if MataUang == "SGD" || MataUang == "sgd" {
+				usd = jumlah * sgd_usd
+			}
+
+			outIDR = usd / idr_usd
+			outPHP = usd / php_usd
+			outTHB = usd / thb_usd
+			outMYR = usd / myr_usd
+			outSGD = usd / sgd_usd
+			outUSD = usd
+			outEUR = usd * usd_eur 
+
+			fmt.Println("----HASIL KONVERSI----")
+			fmt.Printf("Mata Uang Asal: %s %.2f\n", MataUang, jumlah)
+			fmt.Printf("Ke IDR: Rp %.2f\n", outIDR)
+			fmt.Printf("Ke PHP: ₱ %.2f\n", outPHP)
+			fmt.Printf("Ke THB: ฿ %.2f\n", outTHB)
+			fmt.Printf("Ke MYR: RM %.2f\n", outMYR)
+			fmt.Printf("Ke SGD: S$ %.2f\n", outSGD)
+			fmt.Printf("Ke USD: $ %.2f\n", outUSD)
+			fmt.Printf("Ke EUR: € %.2f\n", outEUR)
 		}
 	}
-
-	if MataUang == "IDR" || MataUang == "idr" {
-		usd = jumlah * idr_usd
-	} else if MataUang == "PHP" || MataUang == "php" {
-		usd = jumlah * php_usd
-	} else if MataUang == "THB" || MataUang == "thb" {
-		usd = jumlah * thb_usd
-	} else if MataUang == "MYR" || MataUang == "myr" {
-		usd = jumlah * myr_usd
-	} else if MataUang == "SGD" || MataUang == "sgd" {
-		usd = jumlah * sgd_usd
-	} else {
-		errormsg = "Mata uang yang anda masukkan gak ada"
-	}
-
-	eur = usd * usd_eur
-
-	if errormsg == "" {
-		fmt.Printf("USD: $%.2f\n", usd)
-		fmt.Printf("EUR: €%.2f\n", eur)
-	} else {
-		fmt.Println("izinn:", errormsg)
-	}
+	fmt.Println("Program selesai. Terima kasih")
 }
